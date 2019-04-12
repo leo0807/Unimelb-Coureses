5f861c8e8435a5ae6f083510f090edab3c00bc3b
Scott ZHANG <41654335+leo0807@users.noreply.github.com>
2019-04-12 17:37:46 +1000

    Add files via upload

---
+++ Unimelb-Research Project/Readings/Code/gamma_code.py
@@ -0,0 +1,70 @@
+def unray_single_encode(n):
+      if n < 0 or type(n) != int:
+            return "input num must be positive integer"
+      elif n == 1:
+            return 0
+      else:
+            return(n-1)*"1"+"0"
+def unray_encode(n):
+      new_list =[]
+      if type(n) == list or type(n) == tuple:
+            for num in n:
+                  new_list.append((num-1)*"1"+"0")
+            return new_list
+      else:
+            return unray_single_encode(n)
+#for single input
+def unary_single_decode(n):
+      result = n.count("1")
+      if result == 0:
+            return "1"
+      else:
+            return result+1
+# for list or tuple
+def unray_decode(n):
+      if type(n) == list or type(n) == tuple or type(n) == str:
+            new = []
+            temp = 0
+            for i in n:
+                  if i == "1":
+                        temp += 1
+                  else:
+                        temp += 1
+                        new.append(temp)
+                        temp = 0
+                  
+            return new
+      elif type(n) != str:
+            try:
+                  n = str(n)
+            except TypeError:
+                  print("TypeError")
+            else:
+                  return unary_single_decode(n)
+      else:
+            return unary_single_decode(n)
+
+def gammaCode_encode(n):
+    if type(n) == list or type(n) == tuple:
+        result = []
+        for num in n:
+            num = bin(num)[3:]
+            new = unray_encode(len(num))+num
+            result.append(new)
+    else:
+        n = str(n)
+        num = bin(num)[3:]
+        return unray_encode(len(num))+num
+    return result
+
+def gammaCode_decode(n):
+    result_list = []
+    if type(n) == list or type(n) == tuple:
+        for term in n:
+            new = term.split("0",1)
+            result_list.append(int("1" + new[1], 2))
+    else:
+        n = str(n)
+        new = n.split("0",1)
+        return int("1" + new[1], 2)
+    return result_list
