a569b072f455a74bea426aed0e638cb180b654c2
Scott ZHANG <41654335+leo0807@users.noreply.github.com>
2019-05-02 22:34:55 +1000

    Update Interpolative.py

--- Unimelb-Research Project/Readings/Code/Interpolative.py
+++ Unimelb-Research Project/Readings/Code/Interpolative.py
@@ -1,119 +1,96 @@
+import gamma_code
+from exp_golomb_code import exp_golomb_encode, exp_golomb_decode
 from math import ceil,floor,log
-import re
-
-global symbol,decode
-symbol, decode = False, False
-global total
-total = 0
-
-def Binary_Code(x,lo,hi):
-    print(x)
-    global total
-    results = hi-lo+1
-    if results <= 0:
-        print("math error")
-    else:
-        temp = ceil(log(results,2))
-        print(str(lo)+"<----"+str(x)+"---->"+str(hi)+" :"+str(temp)+'bits')
-    #total += temp
-    #print('Totally consumed'+str(total)+'bits')
-
-def dgaps(L):
-    global symbol
-    if symbol:
-        pass
-    else:
-        for index in range(len(L)):
-            if index > 0:
-                L[index] += int(L[index-1])                
-            else:
-                L[index] = int(L[index])
-        symbol = True
-#print(L)
-    return L
-
-
-def Binary_Interpolative_Code(L,lo,hi):
-# L = dgaps(L)
-# print(L,lo,hi,"input")
-    if type(L) != int:
-        f = len(L)
-    else:
-        f = 1
- 
-#print(str(f)+"times")
-    if f == 0:
-        return 0
-    elif f == 1:
-        return Binary_Code(L,lo,hi)
-    else:
-        h = (f + 1) / 2 
-        f1 = h - 1
-        f2 = f - h
-        
-        L1_indice = int(floor(h-2))
-        L2_indice = int(ceil(h))
-
-#print("L1",L1_indice,"L2",L2_indice)
-        L1 = L[:L1_indice]
-        L2 = L[L2_indice:]
-#In the case like a[2:2], will return []
-        if L1 == []:
-            L1 = L[0]
-#due to the limitation of list, it may happen a[-1：]，a[:0]
-        if L1_indice <= 0:
-            L1 = L[0]
+def binary_code(L,lo,hi):
+    rang = hi - lo + 1
+    if rang == 0:
+        return ""
+    else:   
+      scope = list(range(lo,hi+1))
+      if type(L) ==list:
+            L = L[0]
+      A = L
+      position = scope.index(A)
+     # print("f:",position,"lo:",lo,"hi:",hi)
+     # print("exp_golomb_encode:",exp_golomb_encode(position))
+     # print("decode:",exp_golomb_decode(exp_golomb_encode(position)))
+      return exp_golomb_encode(position)
             
-        if L2 == []:
-            L2 = L[-1]
-        if L2_indice <= 0:
-            L2 = L
-            
-        indice = int(ceil(h-1))
-# print(indice,"this is indice")
-# print(L[indice],lo+f1,hi-f2)
-
-        Binary_Code(L[indice],int(ceil(lo+f1)),int(floor(hi-f2)))
-        Binary_Interpolative_Code(L1,lo,L[indice]-1)
-        Binary_Interpolative_Code(L2,L[indice]+1,hi)
+           
+def interpolative_code(L, start, end):
+    if type(L) == int:
+        length = 0
+    else:
+        length = len(L)
+    if length == 0:
+        return ""
+    elif length == 1:
+        return binary_code(L,start,end)
+    else:
+        mid_term = int(floor(length / 2))
+        increase = mid_term
+        decrease = length - mid_term - 1
+        L1 = L[:mid_term]
+        L2 = L[mid_term+1:]
+    return  (binary_code(L[mid_term],start + increase, end - decrease)
+             + interpolative_code(L1,start,L[mid_term]-1)
+             + interpolative_code(L2,L[mid_term]+1,end) )    
+def former_three(L):
+    length = len(L)
+    start = L[0]
+    end = L[-1]
+    result_list = ( exp_golomb_encode(length) +
+                    exp_golomb_encode(start) +
+                    exp_golomb_encode(end) )
+new_list = []  
+def interpolative_decode(L,length,start,end):
+    if length == 0:
+        #print(L,"length == 0",start,end)
+        return 
+    elif length == 1:
+        try:
+            if end+1 <= L[0]:
+                print(L[0],L,start,end,"lenvt")
+            new_list.append(start + L[0])
+            #new_list.append(range(start,end+1)[L[0]])
+        except:
+            print(L[0],L,start,end,"lenvt")
         return
-
-#while True:         
-start_docid = int(input("Please input the starting ponit of docs: "))
-lo = start_docid 
-end_docid = int(input("Please input the end ponit of docs:"))
-hi = end_docid
-input_list = input("Plesae input the IDs of docs: ")
-range_list = []
-
-
+    else:
+        if L == 0:
+            print("hhhhsah",length,start,end)
+            new_list.append(start)
+            new_list.append(end)
+            return
+        mid_term = int(floor(length / 2))
+        increase = mid_term
+        decrease = length - mid_term - 1
+        #print(start + increase, end - decrease + 1,L[0])
+        split = start + increase + L[0]
+        #split = range(start + increase, end - decrease + 1)[L[0]]
+        new_list.append(split)
+        
+        #print(start + increase, end -decrease + 1,L[0],L)
+        #L1, L2 = [], []
+        L1 = L[1:mid_term+1]
+        L2 = L[mid_term+1:]
+        # if length = 2, then L1 will be L[1:2],that`s wrong, it should be L[1]
+        # L[0] for append ,L[1] for L1, then what for L2
+        '''
+        if length == 2:
+        
+        '''
+        
+        if length == 2:
+            L2 = []
+            #print(L,start,end)
+            #print("L1",L1,increase,start,split - 1,L)
+            #print("L2",L2,decrease,split+1,end,L2 == [])
     
-if input_list == '':
-    print("Please not input empty")
-else:
-    input_list = ''.join(input_list.split())
-    L = list(map(int,input_list))
-    Decode_L = dgaps(L)
-Binary_Interpolative_Code(Decode_L,lo,hi)
-print("total bits cost is: ")
+        if L1 != []:    
+            interpolative_decode(L1,increase,start,split-1)
+        if L2 != []:
+            interpolative_decode(L2,decrease,split+1,end)
+    
+    
 
-'''
-file = input("Please Input the address location of test txt file: ")
-if file == "":
-    print("empty input i")
-try:
-   file_open = open(file,'r')
-   print(file_open.read())
-finally:
-    if file_open:
-        file_open.close()
-def encode(doc_id):
-    new_id = sorted(doc_id)
-    result_list = []
-    for index in range(len(new_id)):
-        if index == 0:
-            result_list.append(new_id[index])
-        else :
-            result_list.append(new_id[index]-new_id[index-1])
-    return result_list
-'''
