d22180cdca4c8142a5f3ec2878b9201f6b1e50a0
Scott ZHANG <41654335+leo0807@users.noreply.github.com>
2019-05-07 22:09:37 +1000

    07/05

--- Unimelb-Research Project/Readings/Code/IO_port.py
+++ Unimelb-Research Project/Readings/Code/IO_port.py
@@ -1,26 +1,39 @@
 from interpolative_code import *
 from numpy import *
 from gen_ran_nums import random_numbers
+import time
+import filecmp
+
 #a = loadtxt('test.txt')
 #L = list(map(int,a))
 #start = L[0]
 #end = L[-1]
-random_numbers(2000000,1700000)  
-with open("sample.txt","r") as r:
+random_numbers(200000,170000)  
+with open("sample.txt","rb") as r:
         input_file = r.readlines()
 temp = loadtxt('sample.txt')
 S = list(map(int,temp))
 #print(S)
 #print("orginal list:",L)
 #str1 = interpolative_code(L,start,end)
+start_time = time.time()
 str1 = interpolative_code(S,S[0],S[-1])
-with open("encode_str.txt","w", encoding = "UTF-8") as f:
-          f.write(str1)
+end_time = time.time()
+print("encoding time:", end_time - start_time)
+with open("encode_str.txt","wb") as f:
+          f.write(bytes(str1,encoding = "utf-8"))
 print("minimun input value:",S[0])
 print("largest input value:",S[-1])
 print("Number of values:",len(S))
 print("total bits:",len(str1))
 decode = exp_golomb_decode(str1)
 interpolative_decode(decode,len(S),S[0],S[-1])
+with open("decode_str.txt","w") as w:
+        #a = map(str,new)
+        for i in new_list:
+                w.write(str(i))
+                w.write(' ')
+      #  w.write(new_list)
 #print("result:",sorted(new_list))
+print(filecmp.cmp("sample.txt","decode_str.txt"))
 print(S == sorted(new_list))
